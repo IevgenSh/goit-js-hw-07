d9057cd641e7cb4e0580bb6510dbac5ee9d24d5d
Ievgen Shakin <shakinzhenya@gmail.com>
2021-12-11 15:42:25 +0200

    7.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+
+};
+
+const galleryItemsString = galleryItems
+  .map(
+    (galleryItem) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${galleryItem.original}">
+      <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+      </a></li>`
+  )
+  .join("");
+
+refs.gallery.insertAdjacentHTML("beforeend", galleryItemsString);
+
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
