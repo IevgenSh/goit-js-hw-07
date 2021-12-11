e377a141352fc5c1ae4fa53be2d786ae7b0980a6
Ievgen Shakin <shakinzhenya@gmail.com>
2021-12-11 02:04:23 +0200

    7.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const instance = basicLightbox.create(`
+  <img src="" width="800" height="600">
+`)
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+  image: instance.element().querySelector('img'),
+
+};
+
+const galleryItemsString = galleryItems
+  .map(
+    (galleryItem) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${galleryItem.original}"><img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"/></a></li>`
+  )
+  .join("");
+refs.gallery.insertAdjacentHTML("beforeend", galleryItemsString);
+
+function onGalleryClick(e) {
+  e.preventDefault();
+  refs.image.src = e.target.dataset.source;
+  instance.show();
+  
+}
+
+function onGalleryEscClick(e) {
+  if (e.keyCode === 27) {
+    instance.close()
+  }
+}
+
+refs.gallery.addEventListener('click', onGalleryClick);
+refs.gallery.addEventListener('keydown', onGalleryEscClick);
+
+
